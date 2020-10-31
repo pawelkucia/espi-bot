35d32bfb9b5d9faa8aabcafc1ae5ed1ad167afe9
pawelkucia79 <pawel.kucia@gmail.com>
2020-10-31 23:22:06 +0100

    [FIX] Read credemtials from yaml config file.

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
 import requests
 import tweepy
+import yaml
 from bs4 import BeautifulSoup
 
 def prepareHashtag(string): 
     return "#" + string.replace(" ", "").replace(".", "")
 
 def sendTweet(tweet):
-    consumer_key = "#"
-    consumer_secret = "#"
-    access_token = "#"
-    access_token_secret = "#"
+    env = yaml.safe_load(open("env.yaml", "r"))
+    consumer_key = env['TWITTER']['API_KEY']
+    consumer_secret = env['TWITTER']['API_KEY_SECRET']
+    access_token = env['TWITTER']['ACCESS_TOKEN']
+    access_token_secret = env['TWITTER']['ACCESS_TOKEN_SECRET']
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     api.update_status(tweet)
 
 def scrap():
-    page = requests.get("http://biznes.pap.pl/pl/reports/espi/all,0,0,0,1")
+    pageUrl = "http://biznes.pap.pl/pl/reports/espi/all,0,0,0,1"
+    page = requests.get(pageUrl)
     soup = BeautifulSoup(page.content, "html.parser")
     table = soup.find(class_="espi")
     rows = table.find_all(class_="inf")
@@ -40,4 +43,5 @@ def scrap():
         print('--------------------------------------------------')
         sendTweet(tweet)
 
+# start scraping
 scrap()
