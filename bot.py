6cb4a159b870ff88102aecda3da9432473fd3aa9
pawelkucia79 <pawel.kucia@gmail.com>
2020-11-08 18:06:53 +0100

    [FEAT] Remember last tweet.

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import requests
 import tweepy
 import yaml
+import hashlib
+import sys
 from bs4 import BeautifulSoup
 
 def prepareHashtag(string): 
@@ -17,7 +19,9 @@ def sendTweet(tweet):
     api = tweepy.API(auth)
     api.update_status(tweet)
 
-def scrap():
+def scrap(lastHash):
+    tweets = []
+    newHash = ""
     pageUrl = "http://biznes.pap.pl/pl/reports/espi/all,0,0,0,1"
     page = requests.get(pageUrl)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -28,20 +32,66 @@ def scrap():
 
     for row in rows:
         link = row.find("a", class_="th")
-        title = link.text.strip()
+        title = link.text.strip().replace("???", "")
         url = link.get("href")
         cols = row.find_all("td")
         time = cols[0].text.strip()
         number = cols[1].text.strip()
         company = cols[2].text.strip()
 
-        hashtags = '#espi ' + prepareHashtag(company)
+        hashtags = '#espi #gpw #NewConnect #giełda #inwestowanie ' + prepareHashtag(company)
 
-        tweet = '[' + today + ' ' + time + '] ' + number + ' ' + company + ' - ' + title + ' ' + url + ' ' + hashtags
+        tweet =  company + ' (' + number + '): ' + title + ' ' + url + ' [' + today + ' ' + time + '] ' + hashtags
         
+        hash = hashlib.md5(tweet.encode('utf-8')).hexdigest()
+        # print(hash)
+        # print(lastHash)
+        # print(newHash)
+        if (hash != lastHash):
+            tweets.append(tweet)
+            if (newHash == ""):
+                newHash = hash
+    
+    if (newHash != ""):
+        saveLastHash(newHash)
+    
+    tweets.reverse()
+    return tweets
+
+def getLastHash():
+    hash = ""
+    hashFile = "lastHash.txt"
+    try:
+        hashdocument = open(hashFile, "r")
+    except IOError:
+        print("Invalid file.")
+        sys.exit()
+    else:
+        hash = hashdocument.readline()
+        hash = hash.replace("\n", "")
+    return hash
+
+def saveLastHash(hash):
+    hashFile = "lastHash.txt"
+    try:
+        hashdocument = open(hashFile, "w")
+    except IOError:
+        print("Invalid file.")
+        sys.exit()
+    else:
+        hashdocument.write(hash)
+        hashdocument.close()
+
+# get last hash from file
+lastHash = getLastHash()
+
+# start scraping
+tweets = scrap(lastHash)
+
+if len(tweets) > 0:
+    for tweet in tweets:
         print(tweet)
         print('--------------------------------------------------')
         sendTweet(tweet)
-
-# start scraping
-scrap()
+else:
+    print('No tweets to post.')
